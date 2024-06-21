bc0e7c9427ece0b853ba4d3a5ecc992b8e5e6bb7
ImranChowdhuryFahim <imran.cuet.cse17@gmail.com>
2024-06-22 04:54:54 +0600

    initialized settings hive box and used in dark mode cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:foodology/core/common/cubits/dark_mode/dark_mode_cubit.dart';
 import 'package:foodology/core/common/widgets/gradient_background.dart';
 import 'package:foodology/core/secrets/app_secrets.dart';
 import 'package:foodology/core/theme/theme.dart';
 import 'package:foodology/features/common/presentation/pages/first_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
@@ -11,6 +14,8 @@ Future<void> main() async {
   await dotenv.load();
   await Supabase.initialize(
       url: AppSecrets.supabaseUrl, anonKey: AppSecrets.supabaseAnonKey);
+  await Hive.initFlutter();
+  await Hive.openBox('settings');
   runApp(const MyApp());
 }
 
@@ -18,16 +23,23 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Foodology',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.dark,
-       builder: (context, child) {
-              return GradientBackground(child: child!);
-            },
-      home: const FirstPage(),
+    return BlocProvider(
+      create: (context) => DarkModeCubit(Hive.box('settings')),
+      child: BlocBuilder<DarkModeCubit, bool>(
+        builder: (context, isDarkMode) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Foodology',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: isDarkMode? ThemeMode.dark : ThemeMode.light,
+             builder: (context, child) {
+                    return GradientBackground(child: child!);
+                  },
+            home: const FirstPage(),
+          );
+        }
+      ),
     );
   }
 }
